27795799c451b5e07443bcebe232ddf8402cb7e9
patrickmoroney17 <patrickmoroney17@gmail.com>
2019-06-26 17:18:17 -0400

    adding functionality to a functioning script

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,48 +3,40 @@ var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
 
 var wins = 0;
 var losses = 0;
-
+var guessCount = 0;
+var guessLeftCount = 9;
+var guessSoFar = [];
 
 // Create variables that hold references to the places in the HTML where we want to display things.
-var directionsText = document.getElementById('directions-text');
 var userChoiceText = document.getElementById('userchoice-text');
 var computerChoiceText = document.getElementById('computerchoice-text');
 var winsText = document.getElementById('wins-text');
 var lossesText = document.getElementById('losses-text');
+// var guessesText = document.getElementById('guesses-text');
 
-// This function is run whenever the user presses a key.
-document.onkeyup = function(event) {
-// Determines which key was pressed.
-var userGuess = event.key;
+for (i = 0; i < 9; i++) {
 
-// if (userGuess !== 'r' || userGuess !== 's' || userGuess !== 'p') {
-//   return false;
-// }
-// Randomly chooses a choice from the options array. This is the Computer's guess.
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-// Reworked our code from last step to use "else if" instead of lots of if statements.
+    document.onkeyup = function(event) {
+    var userGuess = event.key;
 
-// This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-if (userGuess === computerGuess) {
-    wins++;
-    } else {
-    losses++;
+    // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
+    if (userGuess === computerGuess) {
+        wins++;
+        } else {
+            losses++;
+            guessCount++;
+        }
+
+        // Display the user and computer guesses, and wins/losses/ties.
+        userChoiceText.textContent = 'You chose: ' + userGuess;
+        computerChoiceText.textContent = 'The computer chose: ' + computerGuess;
+        winsText.textContent = 'wins: ' + wins;
+        lossesText.textContent = 'losses: ' + losses;
+        // guessesText.textContent = 'Guess Count: ' + guessCount;
     }
-
-    // Hide the directions
-    directionsText.textContent = '';
-
-    // Display the user and computer guesses, and wins/losses/ties.
-    userChoiceText.textContent = 'You chose: ' + userGuess;
-    computerChoiceText.textContent = 'The computer chose: ' + computerGuess;
-    winsText.textContent = 'wins: ' + wins;
-    lossesText.textContent = 'losses: ' + losses;
 };
-
-
-
-
 
 
 // // Creating variables to hold the number of wins, losses, and ties. They start at 0.
