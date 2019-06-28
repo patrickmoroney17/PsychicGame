cc73de9b3ad9d0ca442eb32576d3017fbddd45c5
patrickmoroney17 <patrickmoroney17@gmail.com>
2019-06-27 21:53:30 -0400

    all working with the exception of displaying picked letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,53 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-      alert("the computer choses " + computerGuess);
+alert("the computer choses " + computerGuess);
 
-      var wins = 0;
-      var losses = 0;
-      var remainingGuesses = 9;
-      var guessedLetters = [];
+var wins = 0;
+var losses = 0;
+var remainingGuesses = 9;
+var guessedLetters = [];
 
-      var userChoiceText = document.getElementById("userchoice-text");
-      var computerChoiceText = document.getElementById("computerchoice-text");
-      var winsText = document.getElementById("wins-text");
-      var lossesText = document.getElementById("losses-text");
-      var remainingGuessesText = document.getElementById("remainingGuesses-text");
-      // var guessedLettersText = document.getElementById("guessedLetters-Text")
+var userChoiceText = document.getElementById("userchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var remainingGuessesText = document.getElementById("remainingGuesses-text");
+// var guessedLettersText = document.getElementById("guessedLetters-Text");
 
+document.onkeyup = function(event) {
+  var userChoice = event.key;
 
-      document.onkeyup = function(event) {
-        var userChoice = event.key;
+  if (userChoice === computerGuess) {
 
-        if (userChoice === computerGuess) {
+    alert("You won!");
+    wins++;
+    guessedLetters = [];
+    remainingGuesses = 9;
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    alert("the computer choses " + computerGuess);
 
-          alert("You won!");
-          wins++;
-          guessedLetters = [];
-          remainingGuesses = 9;
-          computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-          alert("the computer choses " + computerGuess);
+  } else {
+    remainingGuesses--;
+    guessedLetters.push(userChoice);
+  }
 
-        } else {
-          remainingGuesses--;
-          guessedLetters.push(userChoice);
+  if (remainingGuesses <= 0) {
 
-        }
+    alert("You lost!");
+    losses++;
+    remainingGuesses = 9;
+    guessedLetters = [];
 
-        if (remainingGuesses <= 0) {
-          // document.getElementById("losses").innerHTML = losses++;
-
-          alert("You lost!");
-          losses++;
-          remainingGuesses = 9;
-          guessedLetters = [];
-
-          computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-          alert("the computer choses " + computerGuess);
-        }
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    alert("the computer choses " + computerGuess);
+  }
 
 
-          userChoiceText.textContent = "You chose: " + userChoice;
-          // computerChoiceText.textContent = "The computer chose: " + computerGuess;
-          winsText.textContent = wins;
-          lossesText.textContent = losses;
-          remainingGuessesText.textContent = remainingGuesses;
-          // guessedLettersText.textContent = guessedLetters;
+    userChoiceText.textContent = "You chose: " + userChoice;
+    winsText.textContent = wins;
+    lossesText.textContent = losses;
+    remainingGuessesText.textContent = remainingGuesses;
+    // guessedLettersText.textContent = guessedLetters;
 
-        }
+  }
