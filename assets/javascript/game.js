db3778dc438484e3cf9cdb7f3ba9c8a52a60d9dd
patrickmoroney17 <patrickmoroney17@gmail.com>
2019-07-14 16:45:12 -0400

    one last push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,63 @@
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-console.log(computerChoice);
+
+console.log("the computer choses " + computerChoice);
 
 var wins = 0;
 var losses = 0;
-var guessesRemaining = 9;
+var guessesRemaining = 8;
+
 var guessedLetters = [];
-// var computerChoice = computerChoice;
-// var letters = "qwertyuiopasdfghjklzxcvbnm"
 
-function jsGuess() {
-  computerChoices[Math.floor(Math.random() * computerChoices.length)];
-  console.log(computerChoice);
-}
+var winsText = $("wins-text");
+var lossesText = $("losses-text");
 
+var guessesRemainingText = $("guessesRemaining-text");
+var userGuessesText = $("userGuesses-text");
 
-document.onkeyup = function (event) {
-      var userGuess = event.key;
-      console.log(userGuess);
+document.onkeyup = function(event) {
 
-      // HANDELING CORRECT GUESSES
-      //test if players guess equals computerChoice, if true it increments wins by 1, and clears used letters array. Supposed to reset guess # to 10 but starts at 9 instead, 
-      if (userGuess === computerChoice) {
-            wins++;
-            guessesRemaining = 10;
-            guessedLetters = [];
+  var userGuess = event.key;
 
-      }
+  if (guessesRemaining <= 0) {
 
-      //HANDELING INCORRECT GUESSES
-      //tests if players guess Does Not Equal computerChoice and decriments guessesRemaining by 1
-      jsGuess();
-      if (userGuess !== computerChoice) {
-            guessesRemaining--;
+    alert("You lost!");
 
-      }
+    losses++;
+    guessesRemaining = 8;
+    guessedLetters = [];
 
-      //when remaining guessesRemaining equals zero, losses is incrimented by 1; guessesRemaining is reset to 10, and used letters array is cleared
-      if (guessesRemaining == 0) {
-            losses++;
-            guessedLetters = []
-            guessesRemaining = 10;
-      }
+    computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log("the computer choses " + computerChoice);
 
-      //HANDELING INCORRECT GUESSES - OUTPUT
-      //this 'if' prevents a letter selected a 2nd time from being written to the guessedLetters again, although it still counts as a guess
-      if (guessedLetters.indexOf(userGuess) >= 0) {
+  }
 
-      } else {
-            //this pushes the players incorrect guess to the guessedLetters and writes it to the HTML
-            guessedLetters.push(userGuess);
-            $(".userGuesses-text").text(guessedLetters);
-            console.log(guessedLetters);
+  if (userGuess === computerChoice) {
 
-      }
-      //OUTPUT TO HTML
-      //these statements write the values/scores generated to the HTML
-      $(".wins-text").text(wins);
-      $(".losses-text").text(losses);
-      $(".guessesRemaining-text").text(guessesRemaining);
+    alert("You won!");
+
+    wins++;
+    guessesRemaining = 8;
+    guessedLetters = [];
+
+    computerChoice = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    console.log("the computer choses " + computerChoice);
+
+  } else {
+
+    guessesRemaining--;
+    console.log("guesses remaining: " + guessesRemaining);
+
+    guessedLetters.push(userGuess);
+
+  }
+
+    $(".wins-text").text(wins);
+    $(".losses-text").text(losses);
+    
+    $(".guessesRemaining-text").text(guessesRemaining);
+    $(".userGuesses-text").text(guessedLetters);
+    console.log(guessedLetters);
 
 }
